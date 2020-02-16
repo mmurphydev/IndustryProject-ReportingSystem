abbe5451a94b8fff5b819e10e7728036b995f130
mmurphydev <mmurphycommerce@gmail.com>
2020-02-16 13:05:17 +0000

    Added; Buttons to homepage, Bootstrap CDN(layouts), API endpoints (Feed, Report)

--- ReportingSystem/routes/index.js
+++ ReportingSystem/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Report Submission page. */
+router.get('/Report', function (req, res, next) {
+  res.render('ReportFault');
+});
+
+/* GET feed page. */
+router.get('/feed', function (req, res, next) {
+  res.render('feed');
+});
+
 module.exports = router;
