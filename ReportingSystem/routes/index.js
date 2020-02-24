5baa3f829ec3eabfe54d95dd077779f3839563d7
mmurphydev <mmurphycommerce@gmail.com>
2020-02-24 10:17:40 +0000

    Created /AddReport route

--- ReportingSystem/routes/index.js
+++ ReportingSystem/routes/index.js
@@ -17,4 +17,37 @@ router.get('/feed', function (req, res, next) {
   res.render('feed');
 });
 
+/*Adding Report to DB*/
+router.post('/AddReport',  function (req, res, next) {
+
+  console.log(req.body);
+  console.log("loggin body: " + req.body.building);
+  console.log("loggin description: " + req.body.description);
+  console.log("loggin room_number: " + req.body.room_number);
+  console.log("lat postionn: " + req.body.latitude);
+  console.log("long position: " + req.body.longitude);
+
+  //create new report with values from body
+  report = new Report({
+    description: req.body.description,
+    building: req.body.building,
+    room_number: req.body.room_number,
+    longitude : req.body.longitude,
+    latitude: req.body.latitude,
+  });
+
+  report.save(function (err, savedReport) {
+    if (err)
+      throw err;
+    res.json({ //Send response with saved details (for testing/debugging)
+      "id": savedReport._id,
+      "building":savedReport.building,
+      "Room Number":savedReport.room_number,
+      "description" : savedReport.description,
+      "longitude" : savedReport.longitude,
+      "latitude" : savedReport.latitude
+    });
+  });
+});
+
 module.exports = router;
