61901b21e3c88bc8c7d2c56f81f791f90eae711c
mmurphydev <mmurphycommerce@gmail.com>
2020-02-24 09:09:08 +0000

    Solves Deprecation Warning. fixes #2

--- ReportingSystem/models/util.js
+++ ReportingSystem/models/util.js
@@ -1,5 +1,5 @@
 var mongoose = require ('mongoose');
 const url = 'mongodb://127.0.0.1:27017/ReportingSystemDB'; //connection string to DB hosted locally
-var connection = mongoose.connect(url);
+var connection = mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology: true} );  //connecting to DB & solving depreciationWarning;
 
 exports.connection=connection;
